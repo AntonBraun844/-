84b89c71aa4618dfe391fe28d04c0d455b4fd1d5
AntonBraun <imperianton@yandex.ru>
2022-08-16 01:08:02 +0300

    домашка вторая

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считавает с консоли строку
+    int result = int.Parse(readValue);      // Преобрадует строку в целое число
+    return result;                          // Возвращает результат
+}
+ int numberWeek;
+numberWeek = Prompt("Введите день недели в виде цифры: ");    
+if (numberWeek<1 || numberWeek>7) {
+ System.Console.WriteLine("вы ввели неверную цифру"); 
+   }
+if (numberWeek>5){
+  System.Console.WriteLine("это выходной день");  
+}
+   else {
+    System.Console.WriteLine("это будний день");
+   }
